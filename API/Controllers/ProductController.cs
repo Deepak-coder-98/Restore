4f25df9d47b70b3d75d33d0830a4b209550e9de6
Deepak-coder-98 <d318300@gmail.com>
2025-09-12 17:58:28 +0530

    created webapi for product of ReStore

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(StoreContext context) : ControllerBase
+    {
+        private readonly StoreContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Product.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
